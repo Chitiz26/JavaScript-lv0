e2ce351b50d716b8efd39b4ec3d050fc2adf92fd
Chitiz Awasthi <Chitizawasthi@gmail.com>
2026-06-06 02:23:34 +0530

    Add files via upload

---
+++ question10.js
@@ -0,0 +1,10 @@
+const myElement = document.getElementById('myDiv');
+
+myElement.textContent = 'Hello, DOM!';
+
+myElement.style.color = 'red';
+
+const button = document.getElementById('myButton');
+button.textContent = 'Click Me!';
+button.style.backgroundColor = 'blue';
+button.style.color = 'white';
